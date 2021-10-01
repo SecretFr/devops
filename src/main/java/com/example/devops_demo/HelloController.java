8d6ff09e741820b09e6242715966923e51667509
SecretFr <linkclean@naver.com>
2021-10-01 11:18:32 +0900

    update

--- src/main/java/com/example/devops_demo/HelloController.java
+++ src/main/java/com/example/devops_demo/HelloController.java
@@ -1,17 +1,29 @@
 package com.example.devops_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloController {
 
+//    @GetMapping("/")
+//    public String hello(){
+//        System.out.println("test");
+//        Calc calc = new Calc();
+//        long result = calc.add(10, 20);
+//
+//        return "Welcome to Simple Ecommerce ver 4.5 (10 + 20 = " + result +")";
+//    }
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    private final String version = "1.0";
+
     @GetMapping("/")
-    public String hello(){
-        System.out.println("test");
-        Calc calc = new Calc();
-        long result = calc.add(10, 20);
-        
-        return "Welcome to Simple Ecommerce ver 4.5 (10 + 20 = " + result +")";
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name), version);
     }
 }
